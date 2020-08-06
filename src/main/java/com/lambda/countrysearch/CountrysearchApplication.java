e04d940e03b46f6bdc0da19a7d4691bdbae69629
David Temple <dstemple7@gmail.com>
2020-08-06 16:21:44 -0500

    init commit - working on it!

---
+++ src/main/java/com/lambda/countrysearch/CountrysearchApplication.java
@@ -0,0 +1,16 @@
+package com.lambda.countrysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountrysearchApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CountrysearchApplication.class,
+            args);
+    }
+
+}
---
+++ src/main/java/com/lambda/countrysearch/controllers/CheckCountry.java
@@ -0,0 +1,8 @@
+package com.lambda.countrysearch.controllers;
+
+import com.lambda.countrysearch.models.Country;
+
+public interface CheckCountry
+{
+    boolean test(Country c);
+}
